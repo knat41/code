d511dda84d42493f9d6cceb2500fb469cd26a18b
ChonkanyaNukoon Network <53162999+cnn2019@users.noreply.github.com>
2020-11-04 13:29:33 +0700

    Create day_in_month.c

---
+++ day_in_month.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int month, year, ad;
+	int day_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	scanf("%d %d", &month, &year);
+	ad = year - 543;
+	if( ((ad % 4 == 0) && (ad % 100 != 0)) || ad % 400 == 0){
+		day_in_month[1] = 29;
+	}
+	printf("%d", day_in_month[month - 1]);
+	return 0;
+}
