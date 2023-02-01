14109b626ef5ae90a965ee50d29bd53c17d9eb7c
knat41 <75963145+knat41@users.noreply.github.com>
2023-02-01 11:32:41 +0700

    Create dec2bin.c
    convert decimal to any base example

---
+++ problem_set/c/dec2bin.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char table[16] = "0123456789ABCDEF";
+	char buffer[30], answer[30] = "";
+	long number;
+	int remainder, base = 2;
+	scanf("%d", &number);
+	if(number == 0) sprintf(answer, "0");
+	while(number > 0){
+		remainder = number % base;
+		number = number / base;
+		strcpy(buffer, answer);
+		sprintf(answer, "%c%s", table[remainder], buffer);
+	}
+	printf("%s", answer);
+	return 0;
+}
